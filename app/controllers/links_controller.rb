376e3d85aed981f42e4395193789b252aafcdfcd
xbearx1987 <xbearx1987@gmail.com>
2017-04-12 15:36:39 +0800

    add link edit/update/destroy

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,13 @@
 class LinksController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
-
+  before_action :find_link_and_check_permission , only: [:edit, :update, :destroy]
 
   def index
     @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params[:id])
   end
 
   def new
@@ -21,7 +25,31 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @link.update(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @link.destroy
+    redirect_to links_path
+  end
+
 private
+  def find_link_and_check_permission
+    @link = Link.find(params[:id])
+
+    if @link.user != current_user
+      redirect_to root_path
+    end
+  end
 
   def link_params
     params.require(:link).permit(:title,:url)
